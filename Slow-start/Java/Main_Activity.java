c8669882641521d9ac4294a464c6169fadced6b0
Harshilbhardwaj47 <edwardkenway200017@gmail.com>
2020-06-11 21:46:50 -0700

    first commit

---
+++ Slow-start/Java/Main_Activity.java
@@ -0,0 +1,15 @@
+package slowstart;
+
+public class Main_Activity {
+	public static void main(String[] args) {
+		int Threshhold, initital_backoff_size, wi, max_backoff_size;
+		initital_backoff_size = 32;
+		max_backoff_size = 1024;
+		
+	}
+
+	public static void Slow_Start() {
+
+	}
+}
+
