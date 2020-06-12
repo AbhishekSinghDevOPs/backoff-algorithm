13ba0a243adba084fd29efeed35815b391b19f5f
Harshilbhardwaj47 <edwardkenway200017@gmail.com>
2020-06-12 15:44:36 -0700

    done slow start function fully operational algorithm

--- Slow-start/Java/Main_Activity.java
+++ Slow-start/Java/Main_Activity.java
@@ -7,8 +7,8 @@ import java.util.Scanner;
 public class Main_Activity {
 	public static void main(String[] args) {
 		int Threshhold, initital_backoff_size, wi, max_backoff_size;
-		initital_backoff_size = 32;
-		max_backoff_size = 1024;
+		initital_backoff_size = 32;// can be changed 
+		max_backoff_size = 1024;// can be changed 
 		
 		int array_ACK[] = new int[5];
 
@@ -23,12 +23,46 @@ public class Main_Activity {
 	public static void Slow_Start(int Threshhold, int initial_backoff_size, int max_backoff_size, int currentbackoff,
 			int[] array_ACK) {
 
+		int backoff_win_size = 0;
+
+		for (int j = 0; j < 5; j++) {
+			if (array_ACK[j] == 1) {
+				System.out.println("Succesfull transmissiom");
+				if (currentbackoff <= max_backoff_size) {
+					if (currentbackoff == initial_backoff_size) {
+						backoff_win_size = initial_backoff_size;
+						System.out.println("Backoff window size is :-" + " " + backoff_win_size);
+					} else {
+						backoff_win_size = currentbackoff / 2;
+						System.out.println("Backoff window size is :-" + " " + backoff_win_size);
+					}
+				} else {
+					backoff_win_size = currentbackoff - initial_backoff_size;
+					System.out.println("Backoff window size is :-" + " " + backoff_win_size);
+				}
+			} else {
+				if (currentbackoff < max_backoff_size) {
+					backoff_win_size = currentbackoff * 2;
+					System.out.println("Backoff window size is :-" + " " + backoff_win_size);
+				} else {
+					System.out.println("Backoff window size is :-" + " " + backoff_win_size);
+				}
+				if (backoff_win_size == max_backoff_size) {
+					backoff_win_size = max_backoff_size;
+					System.out.println("Backoff window size is :-" + " " + backoff_win_size);
+				} else {
+					backoff_win_size = currentbackoff + initial_backoff_size;
+					System.out.println("Backoff window size is :-" + " " + backoff_win_size);
+				}
+			}
+		}
+
 	}
 
 	public static void random_input(int[] array_ACK) {
 		Random rand = new Random();
 		for (int i = 0; i < array_ACK.length; i++) {
-			array_ACK[i] = rand.nextInt(1);
+			array_ACK[i] = rand.nextInt(0);
 		}
 	}
 }
